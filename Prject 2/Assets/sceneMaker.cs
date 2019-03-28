93179bac4ceada59c8b03f913d8e781170d13ccd
rodLouzada <rodrigolouzada1@gmail.com>
2019-03-27 22:46:12 -0400

    Therapist GUI
    Greated the basic, not fully functional GUI for the therapist to set the scene (displayed only on the computer screen);
    Adjusted prefabs;
    Put the position for multiple levels;

---
+++ Prject 2/Assets/sceneMaker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneMaker : MonoBehaviour
+{
+    public Transform p_solid;
+    public Transform p_slab;
+    public Transform p_transparent;
+
+    public Transform t_hanoi;
+    public Transform t_btn;
+    public Transform t_cube;
+
+    public Transform pos2m;
+    public Transform pos5m;
+    public Transform pos10m;
+    public Transform pos20m;
+    public Transform pos30m;
+
+    public int sec;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
