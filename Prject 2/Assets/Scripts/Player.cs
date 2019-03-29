d73d2bae31e712a1298246346f89e6a683383480
Lucas Kessler <ljk30381@uga.edu>
2019-03-29 14:13:25 -0400

    first commit from Lucas
    I started to get some of the headset related scripts, will finish them later today. Just first commit from me

---
+++ Prject 2/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	
+	public Transform head;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.UpArrow)) {
+		  Vector3 newFootPosition = head.position;
+		  newFootPosition = newFootPosition + new Vector3(0,0,1);
+		  teleport(newFootPosition, Vector3.zero);
+	  }
+    }
+	
+	public void teleport(Vector3 playerFeetWorldSpace, Vector3 playerDirectionWorldSpace) {
+		Vector3 currFeetPosTrackingSpace = this.transform.worldToLocalMatrix.MultiplyPoint(head.position);
+		currFeetPosTrackingSpace.y = 0;
+		Vector3 currentFeetPosWorldSpace = this.transform.localToWorldMatrix.MultiplyPoint(currFeetPosTrackingSpace);
+		
+		Vector3 feetOffsetWorldSpace = playerFeetWorldSpace - currentFeetPosWorldSpace;
+		this.transform.Translate(feetOffsetWorldSpace, Space.World);
+		
+	}
+}
---
+++ Prject 2/Assets/Scripts/pressed.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pressed : MonoBehaviour
+{
+    public int timer;
+    public Text txtTimer;
+    public bool psd;
+    
+    void Start()
+    {
+        psd = false;
+        txtTimer.text = "Timer: " + timer;
+        //StartCoroutine(WaitAndPrint());
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "btn")
+        {
+            psd = true;
+
+            StartCoroutine(WaitAndPrint());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "btn")
+        {
+            psd = false;
+            StopCoroutine(WaitAndPrint());
+         }
+    }
+
+    private IEnumerator WaitAndPrint()
+    {
+        while (psd)
+        {
+            yield return new WaitForSeconds(1.0f);
+            if (timer > 0)
+            {
+                txtTimer.text = "Time: " + (timer--);
+            }
+            
+        }
+    }
+}
