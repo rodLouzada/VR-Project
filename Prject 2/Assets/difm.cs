5525d54984afb636bf0e12fd277bed3d1c2d8396
Lucas Kessler <ljk30381@uga.edu>
2019-04-01 22:29:08 -0400

    Finally
    Hooray
    
    Co-Authored-By: rodlouzada <rodlouzada@users.noreply.github.com>

---
+++ Prject 2/Assets/difm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class difm : MonoBehaviour
+{
+    public sceneMaker scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "hand")
+        {
+            scene.itemDB.dificulty = "Difficulty assessment: Medium";
+        }
+    }
+}
---
+++ Prject 2/Assets/ne.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ne : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public sceneMaker scene;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "hand")
+        {
+            scene.itemDB.nausea = "Nausea assessment: Low";
+        }
+    }
+}
+
